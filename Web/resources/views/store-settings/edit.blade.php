bca1fc74fc6c3ee8e9f66e831ff9052cc647c35a
NiraphatK <niraphat23@gmail.com>
2025-09-20 22:21:42 +0700

    Enhance Store Settings form with tooltips for better user guidance

--- Web/resources/views/store-settings/edit.blade.php
+++ Web/resources/views/store-settings/edit.blade.php
@@ -1,5 +1,11 @@
 @extends('home')
 
+@section('css_before')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
+
 @section('content')
     <div class="container mt-4">
         <h3> :: Store Settings :: </h3>
@@ -10,13 +16,17 @@
 
             <!-- Timezone -->
             <div class="form-group row mb-2">
-                <label class="col-sm-3">Time Zone</label>
+                <label class="col-sm-3">
+                    Time Zone
+                    <i class="fa-solid fa-circle-info text-primary" data-bs-toggle="tooltip"
+                        title="กำหนดโซนเวลา เช่น Asia/Bangkok เพื่อใช้ในการคำนวณเวลาการจอง">
+                    </i>
+                </label>
                 <div class="col-sm-6">
                     <select name="timezone" class="form-control">
                         @php
                             $timezones = ['Asia/Bangkok', 'UTC'];
                         @endphp
-
                         @foreach ($timezones as $tz)
                             <option value="{{ $tz }}"
                                 {{ old('timezone', $setting->timezone) == $tz ? 'selected' : '' }}>
@@ -24,16 +34,17 @@
                             </option>
                         @endforeach
                     </select>
-
-                    @if ($errors->has('timezone'))
-                        <div class="text-danger"> {{ $errors->first('timezone') }}</div>
-                    @endif
                 </div>
             </div>
 
             <!-- Cut-off -->
             <div class="form-group row mb-2">
-                <label class="col-sm-3">Cut-off Minutes</label>
+                <label class="col-sm-3">
+                    Cut-off Minutes
+                    <i class="fa-solid fa-circle-info text-primary" data-bs-toggle="tooltip"
+                        title="เวลาปิดรับการจองล่วงหน้าก่อนถึงเวลา เช่น 30 นาที">
+                    </i>
+                </label>
                 <div class="col-sm-3">
                     <input type="number" class="form-control" name="cut_off_minutes"
                         value="{{ old('cut_off_minutes', $setting->cut_off_minutes) }}">
@@ -42,7 +53,12 @@
 
             <!-- Grace -->
             <div class="form-group row mb-2">
-                <label class="col-sm-3">Grace Minutes</label>
+                <label class="col-sm-3">
+                    Grace Minutes
+                    <i class="fa-solid fa-circle-info text-primary" data-bs-toggle="tooltip"
+                        title="เวลาผ่อนผันเมื่อลูกค้ามาสาย เช่น 15 นาที ก่อนเปลี่ยนสถานะเป็น No Show">
+                    </i>
+                </label>
                 <div class="col-sm-3">
                     <input type="number" class="form-control" name="grace_minutes"
                         value="{{ old('grace_minutes', $setting->grace_minutes) }}">
@@ -51,7 +67,12 @@
 
             <!-- Buffer -->
             <div class="form-group row mb-2">
-                <label class="col-sm-3">Buffer Minutes</label>
+                <label class="col-sm-3">
+                    Buffer Minutes
+                    <i class="fa-solid fa-circle-info text-primary" data-bs-toggle="tooltip"
+                        title="เวลาสำหรับทำความสะอาดโต๊ะก่อนรอบถัดไป เช่น 10 นาที">
+                    </i>
+                </label>
                 <div class="col-sm-3">
                     <input type="number" class="form-control" name="buffer_minutes"
                         value="{{ old('buffer_minutes', $setting->buffer_minutes) }}">
@@ -60,7 +81,12 @@
 
             <!-- Slot -->
             <div class="form-group row mb-2">
-                <label class="col-sm-3">Slot Granularity Minutes</label>
+                <label class="col-sm-3">
+                    Slot Granularity Minutes
+                    <i class="fa-solid fa-circle-info text-primary" data-bs-toggle="tooltip"
+                        title="ช่วงเวลาในการจอง เช่น 15 นาทีต่อ 1 slot">
+                    </i>
+                </label>
                 <div class="col-sm-3">
                     <input type="number" class="form-control" name="slot_granularity_minutes"
                         value="{{ old('slot_granularity_minutes', $setting->slot_granularity_minutes) }}">
@@ -69,7 +95,12 @@
 
             <!-- Duration -->
             <div class="form-group row mb-2">
-                <label class="col-sm-3">Default Duration Minutes</label>
+                <label class="col-sm-3">
+                    Default Duration Minutes
+                    <i class="fa-solid fa-circle-info text-primary" data-bs-toggle="tooltip"
+                        title="ระยะเวลาเริ่มต้นที่ลูกค้าสามารถใช้โต๊ะ เช่น 60 นาที">
+                    </i>
+                </label>
                 <div class="col-sm-3">
                     <input type="number" class="form-control" name="default_duration_minutes"
                         value="{{ old('default_duration_minutes', $setting->default_duration_minutes) }}">
@@ -80,20 +111,26 @@
             <div class="form-group row mt-3">
                 <div class="col-sm-6 offset-sm-3 d-flex gap-2">
                     <button type="submit" class="btn btn-primary">Update Settings</button>
-
                     <button type="button" class="btn btn-danger" onclick="resetConfirm()">Reset to Default</button>
                 </div>
             </div>
         </form>
 
-        <!-- ฟอร์ม Reset แยก (hidden) -->
         <form id="reset-form" action="/store-settings/reset" method="post" style="display:none;">
             @csrf
         </form>
     </div>
 
+    <!-- Enable Tooltips -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+            tooltipTriggerList.map(function(tooltipTriggerEl) {
+                return new bootstrap.Tooltip(tooltipTriggerEl)
+            })
+        });
+
         function resetConfirm() {
             Swal.fire({
                 title: 'แน่ใจหรือไม่?',
