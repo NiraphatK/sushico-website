fcf67f04620bf65ddc5de91ecd4d3d5f63106f79
NiraphatK <niraphat23@gmail.com>
2025-09-20 22:40:14 +0700

    Add validation messages and error handling for store settings form fields

--- Web/app/Http/Controllers/StoreSettingController.php
+++ Web/app/Http/Controllers/StoreSettingController.php
@@ -37,21 +37,43 @@ class StoreSettingController extends Controller
     {
         //vali msg 
         $messages = [
+            'timezone.required' => 'กรุณาเลือก Timezone',
+
             'cut_off_minutes.required' => 'กรุณากำหนดค่า Cut-off',
+            'cut_off_minutes.integer'  => 'Cut-off ต้องเป็นตัวเลข',
+            'cut_off_minutes.min'      => 'Cut-off ต้องไม่น้อยกว่า :min นาที',
+            'cut_off_minutes.max'      => 'Cut-off ต้องไม่เกิน :max นาที',
+
             'grace_minutes.required' => 'กรุณากำหนด Grace Period',
+            'grace_minutes.integer'  => 'Grace ต้องเป็นตัวเลข',
+            'grace_minutes.min'      => 'Grace ต้องไม่น้อยกว่า :min นาที',
+            'grace_minutes.max'      => 'Grace ต้องไม่เกิน :max นาที',
+
             'buffer_minutes.required' => 'กรุณากำหนด Buffer',
+            'buffer_minutes.integer'  => 'Buffer ต้องเป็นตัวเลข',
+            'buffer_minutes.min'      => 'Buffer ต้องไม่น้อยกว่า :min นาที',
+            'buffer_minutes.max'      => 'Buffer ต้องไม่เกิน :max นาที',
+
             'slot_granularity_minutes.required' => 'กรุณากำหนด Slot Size',
+            'slot_granularity_minutes.integer'  => 'Slot Size ต้องเป็นตัวเลข',
+            'slot_granularity_minutes.min'      => 'Slot Size ต้องไม่น้อยกว่า :min นาที',
+            'slot_granularity_minutes.max'      => 'Slot Size ต้องไม่เกิน :max นาที',
+
             'default_duration_minutes.required' => 'กรุณากำหนด Duration',
+            'default_duration_minutes.integer'  => 'Duration ต้องเป็นตัวเลข',
+            'default_duration_minutes.min'      => 'Duration ต้องไม่น้อยกว่า :min นาที',
+            'default_duration_minutes.max'      => 'Duration ต้องไม่เกิน :max นาที',
+
         ];
 
         //rule
         $validator = Validator::make($request->all(), [
-            'timezone' => 'required|string',
-            'cut_off_minutes' => 'required|integer|min:1',
-            'grace_minutes' => 'required|integer|min:1',
-            'buffer_minutes' => 'required|integer|min:1',
-            'slot_granularity_minutes' => 'required|integer|min:1',
-            'default_duration_minutes' => 'required|integer|min:1',
+            'timezone'                 => 'required|string',
+            'cut_off_minutes'          => 'required|integer|min:1|max:127',
+            'grace_minutes'            => 'required|integer|min:1|max:127',
+            'buffer_minutes'           => 'required|integer|min:1|max:127',
+            'slot_granularity_minutes' => 'required|integer|min:1|max:127',
+            'default_duration_minutes' => 'required|integer|min:1|max:127',
         ], $messages);
 
         //check 
--- Web/resources/views/store-settings/edit.blade.php
+++ Web/resources/views/store-settings/edit.blade.php
@@ -19,8 +19,7 @@
                 <label class="col-sm-3">
                     Time Zone
                     <i class="fa-solid fa-circle-info text-primary" data-bs-toggle="tooltip"
-                        title="กำหนดโซนเวลา เช่น Asia/Bangkok เพื่อใช้ในการคำนวณเวลาการจอง">
-                    </i>
+                        title="กำหนดโซนเวลา เช่น Asia/Bangkok เพื่อใช้ในการคำนวณเวลาการจอง"></i>
                 </label>
                 <div class="col-sm-6">
                     <select name="timezone" class="form-control">
@@ -34,6 +33,9 @@
                             </option>
                         @endforeach
                     </select>
+                    @if ($errors->has('timezone'))
+                        <div class="text-danger">{{ $errors->first('timezone') }}</div>
+                    @endif
                 </div>
             </div>
 
@@ -42,12 +44,14 @@
                 <label class="col-sm-3">
                     Cut-off Minutes
                     <i class="fa-solid fa-circle-info text-primary" data-bs-toggle="tooltip"
-                        title="เวลาปิดรับการจองล่วงหน้าก่อนถึงเวลา เช่น 30 นาที">
-                    </i>
+                        title="เวลาปิดรับการจองล่วงหน้าก่อนถึงเวลา เช่น 30 นาที"></i>
                 </label>
                 <div class="col-sm-3">
                     <input type="number" class="form-control" name="cut_off_minutes"
                         value="{{ old('cut_off_minutes', $setting->cut_off_minutes) }}">
+                    @if ($errors->has('cut_off_minutes'))
+                        <div class="text-danger">{{ $errors->first('cut_off_minutes') }}</div>
+                    @endif
                 </div>
             </div>
 
@@ -56,12 +60,14 @@
                 <label class="col-sm-3">
                     Grace Minutes
                     <i class="fa-solid fa-circle-info text-primary" data-bs-toggle="tooltip"
-                        title="เวลาผ่อนผันเมื่อลูกค้ามาสาย เช่น 15 นาที ก่อนเปลี่ยนสถานะเป็น No Show">
-                    </i>
+                        title="เวลาผ่อนผันเมื่อลูกค้ามาสาย เช่น 15 นาที ก่อนเปลี่ยนสถานะเป็น No Show"></i>
                 </label>
                 <div class="col-sm-3">
                     <input type="number" class="form-control" name="grace_minutes"
                         value="{{ old('grace_minutes', $setting->grace_minutes) }}">
+                    @if ($errors->has('grace_minutes'))
+                        <div class="text-danger">{{ $errors->first('grace_minutes') }}</div>
+                    @endif
                 </div>
             </div>
 
@@ -70,12 +76,14 @@
                 <label class="col-sm-3">
                     Buffer Minutes
                     <i class="fa-solid fa-circle-info text-primary" data-bs-toggle="tooltip"
-                        title="เวลาสำหรับทำความสะอาดโต๊ะก่อนรอบถัดไป เช่น 10 นาที">
-                    </i>
+                        title="เวลาสำหรับทำความสะอาดโต๊ะก่อนรอบถัดไป เช่น 10 นาที"></i>
                 </label>
                 <div class="col-sm-3">
                     <input type="number" class="form-control" name="buffer_minutes"
                         value="{{ old('buffer_minutes', $setting->buffer_minutes) }}">
+                    @if ($errors->has('buffer_minutes'))
+                        <div class="text-danger">{{ $errors->first('buffer_minutes') }}</div>
+                    @endif
                 </div>
             </div>
 
@@ -84,12 +92,14 @@
                 <label class="col-sm-3">
                     Slot Granularity Minutes
                     <i class="fa-solid fa-circle-info text-primary" data-bs-toggle="tooltip"
-                        title="ช่วงเวลาในการจอง เช่น 15 นาทีต่อ 1 slot">
-                    </i>
+                        title="ช่วงเวลาในการจอง เช่น 15 นาทีต่อ 1 slot"></i>
                 </label>
                 <div class="col-sm-3">
                     <input type="number" class="form-control" name="slot_granularity_minutes"
                         value="{{ old('slot_granularity_minutes', $setting->slot_granularity_minutes) }}">
+                    @if ($errors->has('slot_granularity_minutes'))
+                        <div class="text-danger">{{ $errors->first('slot_granularity_minutes') }}</div>
+                    @endif
                 </div>
             </div>
 
@@ -98,12 +108,14 @@
                 <label class="col-sm-3">
                     Default Duration Minutes
                     <i class="fa-solid fa-circle-info text-primary" data-bs-toggle="tooltip"
-                        title="ระยะเวลาเริ่มต้นที่ลูกค้าสามารถใช้โต๊ะ เช่น 60 นาที">
-                    </i>
+                        title="ระยะเวลาเริ่มต้นที่ลูกค้าสามารถใช้โต๊ะ เช่น 60 นาที"></i>
                 </label>
                 <div class="col-sm-3">
                     <input type="number" class="form-control" name="default_duration_minutes"
                         value="{{ old('default_duration_minutes', $setting->default_duration_minutes) }}">
+                    @if ($errors->has('default_duration_minutes'))
+                        <div class="text-danger">{{ $errors->first('default_duration_minutes') }}</div>
+                    @endif
                 </div>
             </div>
 
@@ -116,6 +128,7 @@
             </div>
         </form>
 
+        <!-- ฟอร์ม Reset แยก -->
         <form id="reset-form" action="/store-settings/reset" method="post" style="display:none;">
             @csrf
         </form>
